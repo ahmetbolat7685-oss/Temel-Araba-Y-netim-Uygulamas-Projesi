213c6b44cb503770cc8add64881c388582eae42f
AHMET BOLAT <abolat@konumsal.com.tr>
2025-08-06 15:13:04 +0300

    Temel Araba Uygulamasi 1- ve 2- 3 -madde geliştirmesi

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         arabalar.add(new Araba(1, "Toyota", "Corolla", 2020, 200000));
         arabalar.add(new Araba(2, "Skoda", "Wolswogen", 2010, 300000));
         arabalar.add(new Araba(3, "Renault", "Clio", 2025, 205000));
-        arabalar.add(new Araba(4, "Wolswogen", "Jetta", 2023, 2014));
+        arabalar.add(new Araba(4, "Wolswogen", "Jetta", 2023, 24000));
         arabalar.add(new Araba(5, "Renault", "Toros", 2024, 396000));
 
         while (true) {// while başlangıç parantezi
@@ -28,6 +28,7 @@ public class Main {
             System.out.print("Seçiminizi Giriniz:");
             int secim = scanner.nextInt();// secime sayı yazabilmek için
             scanner.nextLine();// Eneter boşluk hatası
+
 
             // Tüm Arabalar Listelendi
             if (secim == 1) {
@@ -54,14 +55,43 @@ public class Main {
                     }
 
                 }
-                if (!bulundu) {// bulundu eşit değilse false olur
+                if (!bulundu) {// fiyatı eşit değilse ekrana uyarı verilecek
                     System.out.println("Bu Fiyata Uygun araç  bulunamadı");
                 }
-            }
-            else if (secim ==3){
+            } else if (secim == 3) {// Markaya Göre Ara
 
-            }
 
-        }// while bitiş parantezi
+                System.out.print("Markaya Göre Ara:");
+                String marka = scanner.nextLine();
+
+                boolean bulundu = false;
+
+                for (Araba d : arabalar) {// araba classın içinde arabalar dizisine döndük ve d değişkenine atadık
+                    if (d.marka.equalsIgnoreCase(marka)) { // burada (d dizisinde ki marka ile girilen marka eşit mi diyede kontrol ediliyor)  equalsIgnoreCase :  İki Diziyi karşılaştırırken büyük küçük harf ayrımına takılmaz , ama equals kullanıldığında eşitlenen kelime bire bir aynı olmak zorunadadır
+                        d.yazdir(); // d üzerinde yazdir metodum çalışıyor
+                        bulundu = true;// marka
+                    }
+
+
+                }
+                if (!bulundu){ // marka ismi yanlış girildiyse de bulundu eşit değildir
+                    System.out.println("Aracın Marka Bilgisini Doğru Giriniz");
+                }
+
+                else  if(secim==4)
+                {
+
+                }
+
+
+
+
+
+
+
+
+            }// while bitiş parantezi
+        }
+
     }
-}
+}// class bitiş
