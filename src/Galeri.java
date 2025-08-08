0d4435712883f23592709239bbd08c0387d7431b
AHMET BOLAT <abolat@konumsal.com.tr>
2025-08-08 17:44:12 +0300

    1-Temel Araba Uygulamasi 1- ve 2- 3 - 4- 5- 6- maddelerin geliştirmesi tamamlandı.
    2-Basit Galeri Satış Sistemi projesi 1- 2- maddelerin geliştirmesi yapıldı

---
+++ src/Galeri.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Galeri {
+
+
+
+    ArrayList<Araba> mevcutArabalar = new ArrayList<>();
+
+    ArrayList<String> satisKayitlari = new ArrayList<>();
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+
+        System.out.println("===Galeri Yönetim Sistemi===");
+
+            System.out.print("1- Arabaları Listele");
+            System.out.println();
+            System.out.print("2-Arabayı Satın Al");
+            System.out.println();
+            System.out.print("3-Satışları Görüntele");
+            System.out.println();
+            System.out.print("4-Çıkış");
+            System.out.println();
+            System.out.print("Seçiminizi Giriniz:");
+            int secim = scanner.nextInt();
+
+
+
+        }
+
+
+    }
+
+}
---
+++ src/Müsteri.java
@@ -0,0 +1,15 @@
+public class Müsteri {
+
+    int id;
+    String ad;
+    String soyad;
+
+    public Müsteri(int id, String ad, String soyad) {
+
+        this.id = id;
+        this.ad = ad;
+        this.soyad = soyad;
+
+    }
+
+}
